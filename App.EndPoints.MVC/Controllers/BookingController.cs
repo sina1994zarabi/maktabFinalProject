0b0425cfb11d1df0552a5fae7713a53412920e3c
sina <sina1994zarabi@gmail.com>
2025-03-02 12:39:38 +0330

    CHanged The HomePage LayOut

---
+++ App.EndPoints.MVC/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoints.MVC.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
