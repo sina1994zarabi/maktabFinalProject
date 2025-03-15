0361e5d17b3ff0d13de177c9682ab564cb388938
sina <sina1994zarabi@gmail.com>
2025-03-15 11:47:12 +0330

    Edited HomePgae

--- App.EndPoints.MVC/Controllers/ServicesController.cs
+++ App.EndPoints.MVC/Controllers/ServicesController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Domain.Core.Contract.AppService;
+using App.Domain.Core.Entities.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace App.EndPoints.MVC.Controllers
 {
     public class ServicesController : Controller
     {
+        private readonly IServiceAppService _serviceAppService;
+        private readonly IMemoryCache _memoryCache;
 
-        public IActionResult Index()
+        public ServicesController(IServiceAppService serviceAppService,IMemoryCache memoryCache)
         {
-            return View();
+            _serviceAppService = serviceAppService;
+            _memoryCache = memoryCache;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            string servicesCacheKey = "ServicesCacheKey";
+            if (!_memoryCache.TryGetValue(servicesCacheKey, out List<Service> model))
+            {
+                model = await _serviceAppService.GetAll(default);
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(10));
+                _memoryCache.Set(servicesCacheKey, model, cacheEntryOptions);
+            }
+            return View(model);
+        }
+
+
     }
 }
